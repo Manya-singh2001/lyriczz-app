d932064ec5504f1e8ca079dfd4b1b2eca0ffeaa4
Manya-singh2001 <manyasinghsingh16699@gmail.com>
2023-04-12 22:44:50 +0530

    above

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,19 @@
-const Discover = () => <div>Discover</div>;
+import React from 'react';
+import {Error, Loader, SongCard} from '../components';
+import {genres} from '../assets/constants';
 
-export default Discover;
+const Discover = () => {
+console.log(genres)
+
+  return (
+    <div className='flex flex-col'>
+      <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
+<h2 className='text-bold text-3xl text-white text-left'>Discover</h2>
+sele
+      </div>
+    </div>
+  )
+}
+
+export default Discover
+
